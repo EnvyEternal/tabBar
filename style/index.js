796e881e29a5d551116a3b62850ccaf5e59b5dd0
Eternal <vladultimate@gmail.com>
2021-09-22 11:58:56 +0300

    $

---
+++ style/index.js
@@ -0,0 +1,24 @@
+let body = document.body
+let menu = body.querySelector(".menu");
+let menuItems = menu.querySelectorAll(".menu-item");
+const menuBorder = menu.querySelector(".menu-border");
+let activeItem = menu.querySelector(".active");
+function clickItem(item) {
+    menu.style.removeProperty("--timeOut");
+    if (activeItem == item) return;
+    if (activeItem) {
+        activeItem.classList.remove("active");
+    }
+    item.classList.add("active");
+    activeItem = item;
+    offsetMenuBorder(activeItem, menuBorder); 
+}
+function offsetMenuBorder(element, menuBorder) {
+    const offsetActiveItem = element.getBoundingClientRect();
+    const left = Math.floor(element.offsetLeft - (menuBorder.offsetWidth  - offsetActiveItem.width) / 2) +  "px";
+    menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
+}
+offsetMenuBorder(activeItem, menuBorder);
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => clickItem(item, index));  
+})
